c35661bea1ccdd16a172feac49eacc265a0c109a
yanxu <yanxu@umich.edu>
2024-02-24 18:00:07 +0800

    modified run

--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 import pandas as pd 
 import numpy as np
 import src.hangman as hangman
+import src.tictactoe as tictactoe
 import os
 
 if __name__=="__main__":
-    print('hello world')
-    script_dir = os.path.dirname(__file__)
-    rel_path = 'data/words.txt'
-    file_path = os.path.join(script_dir, rel_path)
+    # print('hello world')
     
-    hangman.main_hangman(file_path)
+    while True:
+        game = input('Please select a game to play: hangman [H] or tictactoe [T]:')
+        if game == 'H':
+            script_dir = os.path.dirname(__file__)
+            rel_path = 'data/words.txt'
+            file_path = os.path.join(script_dir, rel_path)
+            hangman.main_hangman(file_path)
+            break
+        elif game == 'T':
+            tictactoe.printboard()
+            tictactoe.main_tictactoe()
+            break
+        else:
+            print('Wrong input')
